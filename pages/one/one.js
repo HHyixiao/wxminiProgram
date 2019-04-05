b03d78ad76d8beddfd7bf0cec7b43393798b891a
Your Name <you@example.com>
2019-04-06 00:35:07 +0800

    生命周期和路由

--- pages/one/one.js
+++ pages/one/one.js
@@ -16,35 +16,35 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-
+    console.log("监听页面加载")
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function() {
-
+    console.log("监听页面初次渲染完成")
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function() {
-
+    console.log("监听页面显示")
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function() {
-
+    console.log("监听页面隐藏")
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function() {
-
+    console.log("监听页面卸载")
   },
 
   /**
