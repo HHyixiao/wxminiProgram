118dd8450516048c0ac19c72c038c6e5e345fe66
Your Name <you@example.com>
2019-04-08 20:07:31 +0800

    选择器

--- pages/one/one.js
+++ pages/one/one.js
@@ -1,4 +1,22 @@
 // pages/one/one.js
+
+const date = new Date()
+const years = []
+const months = []
+const days = []
+
+for (let i = 1990; i <= date.getFullYear(); i++) {
+  years.push(i)
+}
+
+for (let i = 1; i <= 12; i++) {
+  months.push(i)
+}
+
+for (let i = 1; i <= 31; i++) {
+  days.push(i)
+}
+
 Page({
 
   /**
@@ -8,10 +26,25 @@ Page({
     user: {
       name: "一击男",
       age: "16",
-      sex: "♂"
-    }
+      sex: "♂",
+    },
+    years,
+    year: date.getFullYear(),
+    months,
+    month: 2,
+    days,
+    day: 2,
+    value: [9999, 1, 1]
   },
 
+  bindChange(e) {
+    const val = e.detail.value
+    this.setData({
+      year: this.data.years[val[0]],
+      month: this.data.months[val[1]],
+      day: this.data.days[val[2]]
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
